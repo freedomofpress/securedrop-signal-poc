9b32c94c0127e576993fcce1ad6801b766468108
redshiftzero <jen@freedom.press>
2020-11-25 18:49:01 -0500

    source, journalist: initial e2e messaging working

--- journalist.py
+++ journalist.py
@@ -4,9 +4,7 @@ import time
 
 PAUSE = 2
 
-from signal_protocol import address, curve, identity_key, storage
-
-print("how do you do")
+from signal_protocol import address, curve, identity_key, storage, session, session_cipher, state, protocol
 
 username = "journalist"
 passphrase = "this dont matter"
@@ -21,6 +19,7 @@ time.sleep(PAUSE)
 resp = requests.post('http://127.0.0.1:8081/api/v2/token',
                      data=json.dumps({"username": username, "passphrase": passphrase, "one_time_code": totp}))
 token = resp.json()['token']
+journalist_uuid = resp.json()['journalist_uuid']
 print("we've logged in and gotten our API token!")
 time.sleep(PAUSE)
 
@@ -32,7 +31,7 @@ auth_headers = {
 print("generating required key material for Signal registration...")
 DEVICE_ID = 1 # Unused but required field in the protocol
 registration_id = 666
-journalist_address = address.ProtocolAddress(username, DEVICE_ID)
+journalist_address = address.ProtocolAddress(journalist_uuid, DEVICE_ID)
 
 # Store this somewhere
 identity_key_pair = identity_key.IdentityKeyPair.generate()
@@ -42,6 +41,7 @@ store = storage.InMemSignalProtocolStore(
         identity_key_pair, registration_id
     )
 # Store this somewhere
+signed_pre_key_id = 232
 signed_pre_key_pair = curve.KeyPair.generate()
 signed_pre_key_public = signed_pre_key_pair.public_key().serialize()
 signed_pre_key_signature = (
@@ -51,11 +51,22 @@ signed_pre_key_signature = (
     )
 signed_prekey_timestamp = int(time.time())
 
-signed_pre_key_id = 2123  # TODO prekey id on server
+# prekey = state.PreKeyRecord(pre_key_id, pre_key_pair)
+# store.save_pre_key(pre_key_id, prekey)
+
+signed_prekey = state.SignedPreKeyRecord(
+    signed_pre_key_id,
+    signed_prekey_timestamp,
+    signed_pre_key_pair,
+    signed_pre_key_signature,
+)
+store.save_signed_pre_key(signed_pre_key_id, signed_prekey)
+
 print("done! now attempting registration with the server...")
 resp = requests.post('http://127.0.0.1:8081/api/v2/register',
                      data=json.dumps(
-                         {"signed_prekey": signed_pre_key_public.hex(),
+                         {"signed_prekey_id": signed_pre_key_id,
+                          "signed_prekey": signed_pre_key_public.hex(),
                           "signed_prekey_timestamp": signed_prekey_timestamp,
                           "identity_key": identity_key_pair.public_key().serialize().hex(),
                           "prekey_signature": signed_pre_key_signature.hex(),
@@ -63,3 +74,32 @@ resp = requests.post('http://127.0.0.1:8081/api/v2/register',
                      headers=auth_headers)
 print(resp, resp.text)
 time.sleep(PAUSE)
+
+
+print("waiting for messages from sources")
+while True:
+    time.sleep(PAUSE)
+
+    print("lets see if I have any messages...")
+    resp = requests.get(f'http://127.0.0.1:8081/api/v2/messages',
+                        headers=auth_headers)
+    print(resp, resp.text)
+
+    message = resp.json().get("message", None)
+    source_uuid = resp.json().get("source_uuid", None)
+
+    if not message:
+        continue
+
+    # Else, we have a message.
+    source_address = address.ProtocolAddress(source_uuid, DEVICE_ID)
+    # TODO: Clients need to try_from using the appropriate message type
+    incoming_message = protocol.PreKeySignalMessage.try_from(bytes.fromhex(message))
+
+    plaintext = session_cipher.message_decrypt(
+        store, source_address, incoming_message
+    )
+
+    print(plaintext.decode('utf8'))
+
+    # SECOND ROUNDTRIP, DELETE
--- source.py
+++ source.py
@@ -4,12 +4,10 @@ import time
 
 PAUSE = 2
 
-from signal_protocol import address, curve, identity_key, storage
-
-print("how do you do")
+from signal_protocol import address, curve, identity_key, state, storage, session, session_cipher
 
 # Copy a source codename from the dev console
-passphrase = "seventy alfalfa blustery skied tartly wish freebee"
+passphrase = "strum snowcap monotype unstirred setback hardened unless font"
 
 # Root endpoint
 resp = requests.get('http://127.0.0.1:8080/api/v2/')
@@ -21,7 +19,7 @@ resp = requests.post('http://127.0.0.1:8080/api/v2/token',
                      data=json.dumps({"passphrase": passphrase}))
 print(resp)
 token = resp.json()['token']
-username = resp.json()['source_uuid']
+source_uuid = resp.json()['source_uuid']
 print("we've logged in to the source API and gotten our API token!")
 time.sleep(PAUSE)
 
@@ -33,7 +31,7 @@ auth_headers = {
 print("generating required key material for Signal registration...")
 DEVICE_ID = 1 # Unused but required field in the protocol
 registration_id = 667
-journalist_address = address.ProtocolAddress(username, DEVICE_ID)
+source_address = address.ProtocolAddress(source_uuid, DEVICE_ID)
 
 # Store this somewhere
 identity_key_pair = identity_key.IdentityKeyPair.generate()
@@ -52,15 +50,69 @@ signed_pre_key_signature = (
     )
 signed_prekey_timestamp = int(time.time())
 
-signed_pre_key_id = 2125  # TODO prekey id on server
+signed_pre_key_id = 2125
 print("done! now attempting registration with the server...")
 resp = requests.post('http://127.0.0.1:8080/api/v2/register',
                      data=json.dumps(
-                         {"signed_prekey": signed_pre_key_public.hex(),
+                         {"signed_prekey_id": signed_pre_key_id,
+                          "signed_prekey": signed_pre_key_public.hex(),
                           "signed_prekey_timestamp": signed_prekey_timestamp,
                           "identity_key": identity_key_pair.public_key().serialize().hex(),
                           "prekey_signature": signed_pre_key_signature.hex(),
                           "registration_id": registration_id }),
+                     headers=auth_headers)
+print(resp, resp.text)
+time.sleep(PAUSE)
+
+
+# Get SecureDrop group members, we'll message the members in the default group.
+resp = requests.get('http://127.0.0.1:8080/api/v2/groups',
+                     headers=auth_headers)
+journalist_uuid = resp.json()["default"]
+print(resp, resp.text)
+time.sleep(PAUSE)
+
+
+# Get prekey bundles in preparation for session setup.
+resp = requests.get(f'http://127.0.0.1:8080/api/v2/journalists/{journalist_uuid}/prekey_bundle',
+                     headers=auth_headers)
+journalist_prekey = resp.json()
+print(resp, resp.text)
+time.sleep(PAUSE)
+
+
+# Prepare signal message and send
+# Now in a real situation with groups, this would be "for member in group:"
+journo_signed_prekey = curve.PublicKey.deserialize(bytes.fromhex(journalist_prekey["signed_prekey"]))
+journo_identitykey = identity_key.IdentityKey(bytes.fromhex(journalist_prekey["identity_key"]))
+journalist_address = address.ProtocolAddress(journalist_uuid, DEVICE_ID)
+new_pre_key_bundle = state.PreKeyBundle(
+        journalist_prekey["registration_id"],
+        DEVICE_ID,
+        None,  # for OT prekey, TODO
+        None,  # for OT prekey, TODO
+        journalist_prekey["signed_prekey_id"],
+        journo_signed_prekey,
+        bytes.fromhex(journalist_prekey["prekey_signature"]),
+        journo_identitykey,
+)
+
+session.process_prekey_bundle(
+    journalist_address,
+    store,
+    new_pre_key_bundle,
+)
+
+original_message = "Hello I do declare I have some Interesting Deets about a Bad Thing"
+
+outgoing_message = session_cipher.message_encrypt(
+    store, journalist_address, original_message
+)
+
+print("message baking complete! now sending to my friend via the SecureDrop Server")
+resp = requests.post(f'http://127.0.0.1:8080/api/v2/journalists/{journalist_uuid}/messages',
+                     data=json.dumps(
+                         {"message": outgoing_message.serialize().hex()}),
                      headers=auth_headers)
 print(resp, resp.text)
 time.sleep(PAUSE)
